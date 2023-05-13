0b991797098aba5dbdd020a93bc0b22cb9fa5c83
Jhan Pena <jhsbtian962016@gmail.com>
2023-05-12 19:25:15 -0500

    Actividades

--- Actividad4/Actividad2.py
+++ Actividad4/Actividad2.py
@@ -1,3 +1,10 @@
 def fib (e):
-    for e in range(100):
-        print(fib(e))
+    if e == 0:
+        return 0
+    elif e == 1:
+        return 1
+    else:
+        return fib (e-2) + fib (e-1)
+
+for x in range (50):
+    print (fib(x))
