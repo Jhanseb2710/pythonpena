ddb5ef75e39919897a5120d1088da567ec397463
Jhan Pena <jhsbtian962016@gmail.com>
2023-05-17 09:21:03 -0500

    Actividades

--- Cuantil/Actividad.py
+++ Cuantil/Actividad.py
@@ -1,8 +1,66 @@
 import random
-def llenarLista(rango):
+def llenarLista(tama1, tama2,rango1, rango2):
     lista=[]
-    tam = random.randint (200, 2500)
-    lista=[random.randrange(rango) for i in range(tam)]
+    tama=random.randint(tama1,tama2)
+    for i in range(tama):
+        num=random.randrange(rango1,rango2)
+        lista.append(num)
     return lista
-l1=llenarLista(500)
-print(l1)
+
+def ordenAscendente(lista):
+    for i in range(len(lista)):
+        for j in range(i+1,len(lista)):
+            if lista[i]>lista[j]:
+                aux=lista[i]
+                lista[i]=lista[j]
+                lista[j]=aux
+    return lista 
+
+def cuartilLista(lista):
+    total=[]
+    for i in range(4):
+        i+=1
+        cuartil=round(i*((len(lista)+1)/4),3)
+        total.append(cuartil)
+    return total
+
+def quintilLista(lista):
+    total=[]
+    for i in range(5):
+        i+=1
+        quintil=round(i*((len(lista)+1)/5),3)
+        total.append(quintil)
+    return total
+    
+def sumaLista(lista):
+    sum=0
+    for x in lista:
+        sum+=x
+    return sum
+
+def promedioLista(lista):
+    return sumaLista(lista)/len(lista)
+
+def hallarX(lista1, lista2):
+    total=[]
+    for i in range(len(lista1)):
+        aux1,aux2=0,0
+        if lista1[i]%1 !=0:           
+            aux1=int(lista1[i]//1)-1
+            aux2=aux1+1
+            
+            res=(lista2[aux1]+lista2[aux2])/2
+            total.append(res)
+        elif lista1[i]%1 ==0:
+            res=lista1[i]//1            
+            total.append(res)          
+    return(total)
+
+l1=llenarLista((10),(20),(0),(100))
+print(ordenAscendente(l1))
+print('cuartiles',cuartilLista(l1))
+print('quintiles',quintilLista(l1))
+print('promedio cuartiles',promedioLista(cuartilLista(l1)))
+print('promedio quintiles',promedioLista(quintilLista(l1)))
+print('posicion x en cuartiles',hallarX((cuartilLista(l1)),(ordenAscendente(l1))))
+print('posicion x en quintiles',hallarX((quintilLista(l1)),(ordenAscendente(l1))))
