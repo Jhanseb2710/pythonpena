0ffcfd95b9982359c43c90be8f9529a06c75fea7
Jhan Pena <jhsbtian962016@gmail.com>
2023-06-02 17:26:20 -0500

    Actividades

--- OBJETOS/Persona.py
+++ OBJETOS/Persona.py
@@ -2,6 +2,7 @@ class Persona:
     def __init__(self,nombre,documento):
         self.__nombre=nombre
         self.__documento=documento
+        self.__cursos=[]
     def getNombre(self):
         return self.__nombre
     def setNombre(self,nombre):
@@ -10,9 +11,16 @@ class Persona:
         return self.__documento
     def setDocumento(self,documento):
         self.__documento=documento
+    def getDatos(self):
+        return self.__nombre, self.__documento
+    def getCursos(self):
+        return self.__cursos
+    c=int(input("Ingrese un curso: "), getCursos)
 p=Persona("Andres",315)
 q=Persona("Andrea",412)
 print(p.getNombre())
 print(p.getDocumento())
 print(q.getNombre())
 print(q.getDocumento())
+print(p.getDatos(), q.getDatos())
+print(p.getCursos())
