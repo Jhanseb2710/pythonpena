9be6fdad9e4862349d2650a5937efb82a4f77287
Jhan Pena <jhsbtian962016@gmail.com>
2023-06-11 15:59:48 -0500

    Actividades

--- Diagrama/Empresa.py
+++ Diagrama/Empresa.py
@@ -1,15 +1,17 @@
 from Cliente import *
 from Productos import *
 class Empresa(Cliente):
-    def __init__(self,nombre,telefono,fax):
-        super().__init__(nombre,telefono)
+    def __init__(self,nombre,tipo,descripcion,telefono,fax):
+        super().__init__(nombre,tipo,descripcion)
         self.__nombre = nombre
+        self.__tipo = tipo
+        self.__descripcion = descripcion
         self.__telefono = telefono
         self.__fax = fax
         self.__productos = []
         
     def getDatos(self):
-        return f"{self.__nombre},{self.__telefono},{self.__fax}"
+        return f"{self.__nombre},{self.__tipo},{self.__descripcion},{self.__telefono},{self.__fax}"
     
     def agregarProducto (self,producto):
         self.__productos.append(producto)
@@ -18,7 +20,7 @@ class Empresa(Cliente):
         ob4 = Producto(id,nombre,tipo,descripcion,precio)
         self.__productos.append(ob4) 
         
-    def descuentoProducto (self,precio):
+    def descuentoProducto2 (self,precio):
         self.__precio = precio
         descuento = self.__precio * 0.2
         precio= self.__precio - descuento
--- Diagrama/HerenciaApp.py
+++ Diagrama/HerenciaApp.py
@@ -37,7 +37,7 @@ persona2.componerProducto(3,"Arroz","Grano","Grasas",2000)
 for producto in persona2.getProductos():
     print (f"COMPOSICION {producto.getProducto()}")
 
-persona3 = Individual(27,"Jhan","Individual","empresario","jhsbtian962016@gmail.com", 3133033390, "xra2" )
+persona3 = Individual(27, "Jhan", "Individual", "empresario", "jhsbtian962016@gmail.com", 3133033390, "Cll 62D 75I 53")
 
 print(persona3.getDatos())
 print(isinstance(persona3,Individual))
@@ -47,7 +47,7 @@ for i in persona3.getProductosList():
     print (f"Instanciado de Individual {i.getProducto()}")
 print(persona3.descuentoProducto(1300))
 
-empresa = Empresa("laindustria", 345789632, 5465)
+empresa = Empresa("OTI","Empresa","Maderas",345789632, 5465)
 
 print(empresa.getDatos())
 print(isinstance(empresa,Empresa))
@@ -55,4 +55,4 @@ producto = Producto(1014, "Mueble", "Madera", "objeto", 45000)
 empresa.agregarProducto(producto)
 for i in empresa.getProductosList():
     print (f"Instanciado de Individual {i.getProducto()}")
-print(empresa.descuentoProducto())
+print(empresa.descuentoProducto2())
