04bafc4a48d1f988dd9e190885494362e0f68e0a
Jhan Peña <jhsbtian962016@gmail.com>
2023-07-30 11:41:18 -0500

    Correccion

--- Trimestre 3/Laboratorio.py
+++ Trimestre 3/Laboratorio.py
@@ -1,17 +1,29 @@
 from os import strerror
 
 counters = {chr(ch): 0 for ch in range(ord('a'), ord('z') + 1)}
+non_alpha_numeric_count = 0
+
 file_name = input("Ingresa el nombre del archivo a analizar: ")
 try:
-    file = open(file_name, "rt") 
+    file = open(file_name, "rt")
     for line in file:
         for char in line:
             if char.isalpha():
                 counters[char.lower()] += 1
-    file.close() 
+            elif not char.isnumeric() and not char.isspace():
+                non_alpha_numeric_count += 1
+    file.close()
+
     for char in counters.keys():
         c = counters[char]
         if c > 0:
             print(char, '->', c)
+
+    print("No son alfabeticos ni numericos ->", non_alpha_numeric_count)
+    
 except IOError as e:
-    print("Se produjo un error de E/S: ", strerror(e.errno)) 
+    print("Se produjo un error de E/S: ", strerror(e.errno))
+
+
+
+
